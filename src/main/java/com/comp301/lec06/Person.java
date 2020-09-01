5b87b13fa09ceebff3379ab948dbb8a8900346d1
Aaron J. Smith <onsmith@cs.unc.edu>
2020-09-01 10:16:20 -0400

    Add example code

---
+++ src/main/java/com/comp301/lec06/Person.java
@@ -0,0 +1,14 @@
+package com.comp301.lec06;
+
+public class Person {
+
+  private String name;
+
+  public Person(String name) {
+    this.name = name;
+  }
+
+  public String getName() {
+    return name;
+  }
+}

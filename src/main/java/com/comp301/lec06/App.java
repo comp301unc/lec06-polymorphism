36b097ba40ef3bbb5fe737045889972a4b144593
Aaron J. Smith <onsmith@cs.unc.edu>
2020-09-01 10:12:22 -0400

    Set up project from maven archetype

---
+++ src/main/java/com/comp301/lec06/App.java
@@ -0,0 +1,13 @@
+package com.comp301.lec06;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
